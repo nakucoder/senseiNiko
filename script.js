ef4ab24a85b7e65866f98198733f23be22bbdb8a
Juan N Spinelli <143527164+nakucoder@users.noreply.github.com>
2024-11-22 22:42:57 -0500

    Create script.js
    JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> JavaScript </title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1> Actions </h1>
+  <script src="script.js"></script>
+</body> 
+</html>
